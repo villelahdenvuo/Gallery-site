7ce2c3c74b783ad6bbf6b7d3c3977107d0772e33
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2014-03-15 20:51:33 +0200

    Simple MySQL-test.

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-var restify = require('restify');
+var restify = require('restify')
+	, mysql = require('mysql');
+
+// Load environment variables.
 require('dotenv').load();
 
+
+var db = mysql.createConnection({
+  host     : process.env.MYSQL_SERVER,
+  user     : process.env.MYSQL_USER,
+  password : process.env.MYSQL_PASSWORD
+});
+
 function respond(req, res, next) {
-  res.send('hello ' + req.params.name);
+	db.query('SELECT ? + ? AS solution', [req.params.num1, req.params.num2], function(err, rows, fields) {
+  	if (err) { return next(err); }
+
+  	res.send(rows[0]);
+  	return next();
+	});
 }
 
 var server = restify.createServer();
-server.get('/hello/:name', respond);
-server.head('/hello/:name', respond);
+server.use(restify.gzipResponse());
+
+server.get('/sum/:num1/:num2', respond);
 
 server.listen(process.env.PORT, function() {
   console.log('%s listening at %s', server.name, server.url);
