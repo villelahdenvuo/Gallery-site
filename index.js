d6f598f1236b4fc0b9cdeb643f1d59e0b5b9dacf
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2014-03-15 23:45:19 +0200

    Fix indentation.

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ require('dotenv').load();
 
 
 var db = mysql.createConnection({
-  host     : process.env.MYSQL_SERVER,
-  user     : process.env.MYSQL_USER,
-  password : process.env.MYSQL_PASSWORD
+	host:     process.env.MYSQL_SERVER,
+	user:     process.env.MYSQL_USER,
+	password: process.env.MYSQL_PASSWORD
 });
 
 function respond(req, res, next) {
 	db.query('SELECT ? + ? AS solution', [req.params.num1, req.params.num2], function(err, rows, fields) {
-  	if (err) { return next(err); }
+		if (err) { return next(err); }
 
-  	res.send(rows[0]);
-  	return next();
+		res.send(rows[0]);
+		return next();
 	});
 }
 
@@ -26,5 +26,5 @@ server.use(restify.gzipResponse());
 server.get('/sum/:num1/:num2', respond);
 
 server.listen(process.env.PORT, function() {
-  console.log('%s listening at %s', server.name, server.url);
+	console.log('%s listening at %s', server.name, server.url);
 });
