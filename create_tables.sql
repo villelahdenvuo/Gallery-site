3855081ac1fbbbe97aec182b8186c902333cec9f
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2014-03-23 21:25:36 +0200

    Work on HTML.

--- create_tables.sql
+++ create_tables.sql
@@ -1,19 +1,17 @@
 CREATE TABLE gal_folder(
-	id INT NOT NULL AUTO_INCREMENT,
-	PRIMARY KEY (id),
-	name VARCHAR(255) CHARACTER SET utf8,
-	parent_id INT,
-	INDEX parent_index (parent_id),
-	FOREIGN KEY (parent_id)
-		REFERENCES gal_folder(id)
-		ON DELETE CASCADE -- Delete folder if parent folder is deleted.
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(255) NOT NULL,
+	lft INT NOT NULL,
+	rgt INT NOT NULL	
+	INDEX lft_index (lft),
+	INDEX rgt_index (rgt),
+	INDEX child_index (lft, rgt),
 ) ENGINE=INNODB;
 
 CREATE TABLE gal_photo(
-	id INT NOT NULL AUTO_INCREMENT,
-	PRIMARY KEY (id),
-	name VARCHAR(255) CHARACTER SET utf8,
-	path VARCHAR(255) ASCII,
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(255),
+	path VARCHAR(255),
 	folder_id INT NOT NULL,
 	INDEX folder_index (folder_id),
 	FOREIGN KEY (folder_id)
---
+++ www/js/main.js
@@ -0,0 +1,55 @@
+$(function () {
+
+HEIGHTS = [];
+
+function getheight(images, width) {
+  width -= images.length * 5;
+  var h = 0;
+  for (var i = 0; i < images.length; ++i) {
+    h += $(images[i]).data('width') / $(images[i]).data('height');
+  }
+  return width / h;
+}
+
+function setheight(images, height) {
+  HEIGHTS.push(height);
+  for (var i = 0; i < images.length; ++i) {
+    $(images[i]).css({
+      width: height * $(images[i]).data('width') / $(images[i]).data('height'),
+      height: height
+    });
+    $(images[i]).attr('src', $(images[i]).attr('src').replace(/w[0-9]+-h[0-9]+/, 'w' + $(images[i]).width() + '-h' + $(images[i]).height()));
+  }
+}
+
+function resize(images, width) {
+  setheight(images, getheight(images, width));
+}
+
+function run(max_height) {
+  var size = window.innerWidth - 50;
+
+  var n = 0;
+  var images = $('.photo-container img');
+  w: while (images.length > 0) {
+    for (var i = 1; i < images.length + 1; ++i) {
+      var slice = images.slice(0, i);
+      var h = getheight(slice, size);
+      if (h < max_height) {
+        setheight(slice, h);
+        n++;
+        images = images.slice(i);
+        continue w;
+      }
+    }
+    setheight(slice, Math.min(max_height, h));
+    n++;
+    break;
+  }
+  console.log(n);
+}
+
+window.addEventListener('resize', function () { run(250); });
+$(function () { run(250); });
+
+});
