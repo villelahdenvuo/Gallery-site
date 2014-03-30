8ca22564120bd384adbc36b428c130288ee5895b
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2014-03-30 20:14:07 +0300

    Fine tune sql.

--- api/sql/add_test_data.sql
+++ api/sql/add_test_data.sql
@@ -5,17 +5,17 @@ INSERT INTO gal_folder (name, lft, rgt, password, description) VALUES
 	('Views', '4', '5', NULL, 'Beautiful views');
 
 INSERT INTO gal_photo (name, path, description, folder_id) VALUES
-	('Funny picture 1', 'http://community.giffgaff.com/t5/image/serverpage/image-id/38906iC12B6E7DDEBCED42/image-size/original', 'Customer support', 0);
+	('Funny picture 1', 'http://community.giffgaff.com/t5/image/serverpage/image-id/38906iC12B6E7DDEBCED42/image-size/original', 'Customer support', 1);
 
 INSERT INTO gal_tag (name, description) VALUES
 	('funny', 'the picture brings happyness (yes happyness) to the world.'),
 	('doge', 'Wow, such photon, so light.');
 
 INSERT INTO gal_taglist (tag_id, photo_id) VALUES
-	(1, 2);
+	(1, 7);
 
-INSERT INTO gal_user (name) VALUES
-	('admin');
+INSERT INTO gal_user (id, name, admin) VALUES
+	('1247782261', 'Ville Lahdenvuo', 1);
 
 INSERT INTO gal_rating (score, user_id, photo_id) VALUES
-	(5, 1, 2);
+	(5, '1247782261', 7);
