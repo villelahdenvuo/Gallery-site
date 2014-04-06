b14fdd1641088ef678c4be6854e18eead3586644
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2014-04-07 02:53:13 +0300

    Refactor routes code.

--- api/routes.js
+++ api/routes.js
@@ -1,31 +1,30 @@
 module.exports = function (server, routes, verify) {
 
-	get('/photos', 'photos#index');
-	get('/photo/:id', 'photos#show');
-	del('/photo/:id', verify({ admin: true }), 'photos#destroy');
+	get ('/photos',    'photos#index');
+	get ('/photo/:id', 'photos#show');
+	del ('/photo/:id', verify({ admin: true }), 'photos#destroy');
 	post('/photo/:id', verify({ admin: true }), 'photos#save');
-	put('/photo', verify({ admin: true }), 'photos#create');
+	put ('/photo',     verify({ admin: true }), 'photos#create');
 
 	get('/users', 'users#index');
-	post('/user/verify', verify(), 'users#log');
+	//post('/user/verify', verify(), 'users#log');
 	get('/user/:id', 'users#show');
 
 
 	// Route helpers.
-	function use(method, route, controllers) {
-		params = controllers.map(function (c) {
-			if (typeof c === 'string') {
-				var parts = c.split('#');
-				return routes[parts[0]][parts[1]];
-			}
-			return c;
+	function get (r) { use.call(0, 'get',  r, [].slice.call(arguments, 1)); }
+	function post(r) { use.call(0, 'post', r, [].slice.call(arguments, 1)); }
+	function del (r) { use.call(0, 'del',  r, [].slice.call(arguments, 1)); }
+	function put (r) { use.call(0, 'put',  r, [].slice.call(arguments, 1)); }
+
+	function use(method, route, middleware) {
+		var p = middleware.map(function (c) {
+			if (typeof c === 'string') { // string -> controller#method
+				return routes[c.split('#')[0]][c.split('#')[1]];
+			} else { return c; }
 		});
-		params.unshift(route);
-		console.log('Routing', method.toUpperCase(), route, '->', controllers);
-		server[method].apply(server, params);
+		console.log('Routing', method.toUpperCase(), route, '->', middleware);
+		p.unshift(route);
+		server[method].apply(server, p);
 	}
-	function get(route) { use.call(null, 'get', route, [].slice.call(arguments, 1)); }
-	function post(route) { use.call(null, 'post', route, [].slice.call(arguments, 1)); }
-	function del(route) { use.call(null, 'del', route, [].slice.call(arguments, 1)); }
-	function put(route) { use.call(null, 'put', route, [].slice.call(arguments, 1)); }
 };
