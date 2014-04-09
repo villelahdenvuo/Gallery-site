3f8be90b455a227fb93b69f630ae8e8817485dac
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2014-04-10 00:53:39 +0300

    Layout stuff

--- www/js/photo.js
+++ www/js/photo.js
@@ -22,98 +22,40 @@
   }])
 
   photo.config(function ($stateProvider) {
-    function handleScroll(e) {
-      e.preventDefault();
-      e.stopPropagation();
-    }
-
-    function stopScroll() {
-      angular.element(document.body).on('mousewheel', handleScroll);
-    }
-
-    function startScroll() {
-      angular.element(document.body).off('mousewheel', handleScroll);
-    }
-
-
     $stateProvider.state("photos", {
       url: '/',
       templateUrl: "views/photo/index.html",
       controller: 'PhotosController'
-      //controllerAs: 'PhotosController'
     });
 
     $stateProvider.state('photos.photo', {
       url: 'photo/{id:[0-9]{1,10}}',
-      views: {
-        "photo": { templateUrl: 'views/photo/show.html', controller: 'PhotoController' }
-      },
+      templateUrl: 'views/photo/show.html',
+      controller: 'PhotoController',
       onEnter: stopScroll,
       onExit: startScroll
-      /*onEnter: function($log, $state, $modal) {
-        $log.debug('showing photo modal');
-        $modal.open({
-          templateUrl: 'views/photo/show.html',
-          windowClass: 'photo',
-          controller: ['$stateParams', '$scope', 'Photo', function ($stateParams, $scope, Photo) {
-            $scope.photo = Photo.get({ id: $stateParams.id });
-            $scope.editing = 'fa-edit';
-
-            $scope.edit = function () {
-              if ($scope.editing === 'fa-edit') {
-                $scope.editing = 'fa-save';
-              } else {
-                $scope.photo.description = cleanString($scope.photo.description);
-                $scope.photo.name = cleanString($scope.photo.name);
-                $scope.photo.$save(function () {
-                  $log.debug('changes saved');
-                  $scope.editing = 'fa-edit';
-                });
-              }
-            };
-
-            $scope.keydown = function ($event) {
-              if ($event.which !== 13) { return; }
-              // On enter, save changes.
-              $scope.edit();
-              $scope.editing = 'fa-edit';
-            };
-
-            $scope.destroy = function () {
-              $scope.photo.$delete(function () {
-                $log.info('deleted photo', $scope.photo);
-                $scope.$close();
-                $state.go('home', {}, { reload: true });
-              });
-            };
-          }]
-        }).result.catch(function () {
-          $log.debug('photo dismissed');
-          $state.go('home');
-        });
-      }*/
     });
 
-    $stateProvider.state('home.new', {
+    $stateProvider.state('photos.new', {
       url: 'photo/new',
       onEnter: function($log, $state, $modal) {
         $log.debug('showing new photo modal');
         $modal.open({
           templateUrl: 'views/photo/new.html',
           windowClass: 'new-photo',
-          controller: ['$stateParams', '$scope', 'Photo', function ($stateParams, $scope, Photo) {
+          controller: ['$scope', 'Photo', function ($scope, Photo) {
             $scope.photo = {};
 
             $scope.submit = function () {
               Photo.create($scope.photo, function () {
                 $scope.$close();
-                $state.go('home', {}, { reload: true });
+                $state.go('photos', {}, { reload: true });
               });
             }
           }]
         }).result.catch(function () {
           $log.debug('new photo modal dismissed');
-          $state.go('home');
+          $state.go('photos');
         });
       }
     });
@@ -150,7 +92,6 @@
     $scope.destroy = function () {
       $scope.photo.$delete(function () {
         $log.info('deleted photo', $scope.photo);
-        $scope.$close();
         $state.go('home', {}, { reload: true });
       });
     };
@@ -173,6 +114,7 @@
   });
 
   function cleanString(str) {
+    if (!str) { return ''; }
     return S(str).stripTags().decodeHTMLEntities().collapseWhitespace().s;
   }
 
@@ -196,10 +138,6 @@
     }
   }
 
-/*  function resize(images, width) {
-    setheight(images, getheight(images, width));
-  }*/
-
   function layout(max_height) {
     var size = window.innerWidth - 15;
     var n = 0;
@@ -219,6 +157,14 @@
       n++;
       break;
     }
+  }
+
+  function stopScroll() {
+    angular.element(document.body).addClass('modal-open');
+  }
+
+  function startScroll() {
+    angular.element(document.body).removeClass('modal-open');
   }
 
   window.addEventListener('resize', function () { layout(MAX_HEIGHT); });
