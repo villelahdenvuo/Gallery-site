0c86f03821bade4d00c0dad7855b828419317fb6
Ville Lahdenvuo <ville.lahdenvuo@luotta.fi>
2015-09-24 20:21:52 +0300

    Update 2015-09-24T17:21:43.691Z

--- api/sql/drop_tables.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS gal_rating;
-DROP TABLE IF EXISTS gal_user;
-DROP TABLE IF EXISTS gal_taglist;
-DROP TABLE IF EXISTS gal_tag;
-DROP TABLE IF EXISTS gal_photo;
-DROP TABLE IF EXISTS gal_folder;
--- app.js
+++
@@ -1,5 +0,0 @@
-var restify = require('restify')
-	, request = require('request')
-	, mysql = require('mysql');
-
-require('./api')(restify, request, mysql);
